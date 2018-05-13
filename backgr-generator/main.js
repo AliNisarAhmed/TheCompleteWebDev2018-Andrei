c1d511749ab10b39fe67d3afd4b3a2a77007e84b
AliNisarAhmed <ali.nisar87@gmail.com>
2018-05-13 19:56:09 +0500

    complete: only body.style.backgr remaining

--- backgr-generator/main.js
+++ backgr-generator/main.js
@@ -1,9 +1,13 @@
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const css = document.querySelector("h3");
-const body = document.querySelector("#gradient");
+const body = document.body;
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
 
-css.textContent = body.style.background + ";";
+
+// console.log(document.body.style.background)   // Why is this empty???
+// css.textContent = body.style.background + ";";
 
 function setGradient() {
     body.style.background = 
@@ -11,7 +15,36 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function randomInt() {
+    return (Math.round(Math.random() * 256)) + 1; 
+}
+
+function randomHex() {
+    let hex = randomInt().toString(16);
+    if (hex.length < 2) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
+
+function randomColor() {
+    return `#${randomHex()}${randomHex()}${randomHex()}`;
+}
+
 
 color1.addEventListener("input", setGradient); 
 
 color2.addEventListener("input", setGradient);
+
+button1.addEventListener("click", function() {
+    color1.value = randomColor();
+    setGradient();
+});
+
+button2.addEventListener("click", function() {
+    color2.value = randomColor();
+    setGradient();
+})
+
+
