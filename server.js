9aee7d0a2589a6c24bb02002a357982b60da71dd
AliNisarAhmed <ali.nisar87@gmail.com>
2018-05-25 22:25:12 +0500

    node practice

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.get('/', (req, res) => {
+    res.send('getting root');
+})
+
+app.get('/profile', (req, res) => {
+    res.send('getting profile');
+})
+
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    res.send('success');
+});
+
+app.listen(3000);
