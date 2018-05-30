fd1f9183e0deb94c6dd6e6aa14755aedf1fe27b4
AliNisarAhmed <ali.nisar87@gmail.com>
2018-05-30 22:55:57 +0500

    post security

--- facerecog_app/face-recog-api/Controllers/image.js
+++ facerecog_app/face-recog-api/Controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'd27038cccdb34d8ba37b9cec2a40147a'
+ });
+
+ const handleApiCall = (req, res) => {
+  app.models.predict(
+  Clarifai.FACE_DETECT_MODEL, 
+  req.body.input)
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => res.status(400).json('unable to work with api'))
+ };
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   
@@ -11,5 +27,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 }
